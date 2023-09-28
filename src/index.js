6a0d018f6af9d29a53c0060805c2b7529976a987
Lim200 <112889518+Lim200@users.noreply.github.com>
2023-09-28 13:41:54 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const amountElement = 10;
+
+    const resultArray = expr.match(new RegExp(`.{1,${amountElement}}`, 'g'));
+    // console.log(resultArray);
+
+    const morseCode = resultArray.map(sequence => {
+        return sequence
+          .replace(/10/g, '.') // Заменяем 0 на точку
+          .replace(/11/g, '-') // Заменяем 1 на тире
+          .replace(/00/g, '')  // Заменяем 1 на пусто
+          .replace(/\*{10}/g, ' '); // Заменяем на пробел
+      });
+
+      let resultString = '';
+
+for (const item of morseCode) {
+    // Проверяем, есть ли такой элемент в MORSE_TABLE
+    if (MORSE_TABLE.hasOwnProperty(item)) {
+        // Если есть, добавляем его значение в строку
+        resultString += MORSE_TABLE[item];
+    } else if (item === ' ') {
+        resultString += ' ';
+    }
+}
+
+return resultString;
 }
 
 module.exports = {
